030ede8ebd8fe7603239e912989a64f8a36bce48
bella0413 <51810552+bella0413@users.noreply.github.com>
2020-11-01 03:52:08 +0900

    component class and function practice

--- tart-test/src/App.js
+++ tart-test/src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import Subject from "./components/Subject";
+import Hello from "./components/Hello";
+
 
 
 function App() {
   const subject = "react"; 
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,7 +25,10 @@ function App() {
           Learn React
         </a>
       </header>
-      
+      <Subject title="Nice" content="hahahaha success"/>
+      <Hello />
+      <Hello></Hello>
+      <Subject></Subject>
     </div>
 
   );
